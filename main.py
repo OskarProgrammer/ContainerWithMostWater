3409df195c31fc3f05e860610a94fd7787ee2476
Oskar <Zaden>
2023-05-14 00:52:21 +0200

    Adding file main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        lista=[0]
-        for x in range(len(height)):
-            for j in range(len(height)):
-                if height[x] < height[j]:
-                    pole = height[x] * abs(x-j)
-                else:
-                    pole = height[j] * abs(x-j)
-                if pole > lista[0]:
-                    lista[0]=pole
-                else:
-                    continue
-        return max(lista)
+        left,right,answer=0,len(height)-1,0
+        while left<=right:
+            area=min(height[right],height[left])*(right-left)
+            answer=max(answer,area)
+            if height[right]>height[left]:
+                left+=1
+            else:
+                right-=1
+        return answer
